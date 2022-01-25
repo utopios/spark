4773bf1bdfae63d8f7d3cc060e6eb68f4d0f0e78
ihab abadi <utopios@imac-de-ihab.home>
2022-01-25 16:29:29 +0100

    suite streaming output

--- J1/src/main/java/MainStream.java
+++ J1/src/main/java/MainStream.java
@@ -5,6 +5,12 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.StreamingContext;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import receivers.JavaCustomReceiver;
+import scala.reflect.ClassTag;
 import tools.SpContext;
 import static org.apache.spark.sql.functions.*;
 public class MainStream {
@@ -26,9 +32,29 @@ public class MainStream {
         //Socket
         //Dataset datasetSocket = SpContext.getSession().readStream().format("socket").option("host", "localhost").option("port", 7777).load();
         //Kafka
-        Dataset datasetKafka = SpContext.getSession().readStream().format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "test").load();
+        /*Dataset datasetKafka = SpContext.getSession().readStream().format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "test").load();
         personneDataset.groupBy("numberFriends").count().writeStream().format("console").outputMode("complete").start().awaitTermination();
 
+        //CustomReceiver
+        JavaStreamingContext streamingContext = new JavaStreamingContext(SpContext.getContext(), Duration.apply(1));
+        JavaReceiverInputDStream customDataset = streamingContext.receiverStream(new JavaCustomReceiver("localhost", 7777));
+        //Appliquer les transformations
+        streamingContext.start();
+        streamingContext.awaitTermination();*/
 
+        //output
+        // Console
+        //personneDataset.writeStream().format("console").outputMode("update").start().awaitTermination();
+
+        //kafka
+        //personneDataset.writeStream().outputMode("update").format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("topic", "sendTest").start().awaitTermination();
+
+        //foreachBatch
+        personneDataset.writeStream().outputMode("update").foreachBatch((dataset, id) -> {
+            dataset.foreach(p -> {
+                //Call api
+                System.out.println(p.toString());
+            });
+        }).start().awaitTermination();
     }
 }
