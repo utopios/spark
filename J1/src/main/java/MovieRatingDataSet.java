95f6b212a9a544066c17c9eee467c915f2e9101c
ihab abadi <utopios@imac-de-ihab.home>
2022-01-25 10:11:38 +0100

    suite dataSet à partir d'une rdd

--- J1/src/main/java/MovieRatingDataSet.java
+++ J1/src/main/java/MovieRatingDataSet.java
@@ -1,3 +1,4 @@
+import models.MovieName;
 import models.MovieRate;
 import org.apache.arrow.flatbuf.Int;
 import org.apache.spark.api.java.JavaRDD;
@@ -9,11 +10,19 @@ import tools.SpContext;
 
 public class MovieRatingDataSet {
 
+    private JavaSparkContext context;
+    private SparkSession session;
+
+    public MovieRatingDataSet(JavaSparkContext context, SparkSession session) {
+        this.context = context;
+        this.session = session;
+    }
+
     //Fonction pour récupérer un dataSet qui contient movieRate (movieId, rate) => film.data
     public  Dataset<MovieRate> getRatingDataSet() {
-        JavaSparkContext context = SpContext.getContext();
-        SparkSession session = SpContext.getSession();
-        JavaRDD<MovieRate> rddMovieRate = SpContext.getContext().textFile("data/film.data").map(r -> {
+        /*JavaSparkContext context = SpContext.getContext();
+        SparkSession session = SpContext.getSession();*/
+        JavaRDD<MovieRate> rddMovieRate = context.textFile("data/film.data").map(r -> {
             String[] data = r.split("\t");
             return  new MovieRate(new Integer(data[1]), new Integer(data[2]));
         });
@@ -23,4 +32,12 @@ public class MovieRatingDataSet {
     }
 
     //Fonction pour récupérer un dataSet qui contient movieName (movieId, name) => names.item
+    public Dataset<MovieName> getMovieNameDataSet() {
+        JavaRDD<MovieName> rddMovieName = context.textFile("data/names.item").map( r-> {
+            String[] data = r.split(",");
+            return new MovieName(new Integer(data[0]), data[1]);
+        });
+        Dataset<MovieName> movieNameDataset = session.sqlContext().createDataset(rddMovieName.rdd(), Encoders.bean(MovieName.class));
+        return movieNameDataset;
+    }
 }
